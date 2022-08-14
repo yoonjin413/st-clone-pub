45202f2aef71440703d56378d71b916d8bd9c0c7
smc045 <smc045@stunitas.com>
2022-08-14 19:52:58 +0900

    [SKYEDU-7343]2023 PRE반수반

--- SOBANG-2523/web/index.js
+++ SOBANG-2523/web/index.js
@@ -2,5 +2,18 @@ $(function() {
 
     setTimeout(function() {
         console.log('this is SOBANG-2523');
+
+        $('.tab_menu li:eq(0) > a').trigger('click');
     }, 100);
+
+    $('.tab_menu > li > a').on('click', function() {
+        const idx = $(this).parent('li').index();
+
+        $('.sect03 ul[class*="tab_"] > li').removeClass('active');
+
+        $('.tab_menu > li:eq('+idx+')').addClass('active');
+        $('.tab_cont > li:eq('+idx+')').addClass('active');
+    });
 });
+
+

8229e96a69d752989130bf29e9e5f14b7dc1ef3d
smc045 <smc045@stunitas.com>
2022-09-04 17:02:46 +0900

    [GONG-8723]교정학, 또 1위 김지훈

--- GONG-8723/web/index.js
+++ GONG-8723/web/index.js
@@ -9,34 +9,26 @@ $(function() {
     $('.sect01 .cont_quote > ul').bxSlider({
         mode: "vertical",
         auto: true,
-        // ticker: true,
         pager: false,
         controls: false,
-        speed: 3000,
-        // pause: 3000,
+        speed: 2500,
         minSlides: 1,
         slideMargin: 96,
         useCSS: true,
     });
 
     $('.sect01 .cont_review > ul').bxSlider({
-        // auto: true,
-        // ticker : true,
-        // useCSS : true,
-        // speed : 3000,
-        // // pause: 3000,
-        // minSlides:1,
-        // maxSlides: 50,
-        // moveSlides: 1,
-        // slideWidth: 700,
-
-        minSlides: 1,
-        maxSlides: 4,
-        moveSlides: 1,
-        slideWidth:700,
-        useCSS : 3000,
-        speed : 40000,
+        pager: false,
+        controls: false,
         ticker : true,
-        //tickerHover : true
+        speed : 25000,
+        slideMargin: 18,
+    });
+
+    $('.sect03 .slide_review').bxSlider({
+        pager: false,
+        controls: false,
+        speed: 25000,
+        ticker: true,
     });
 });

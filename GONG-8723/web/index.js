d5036e04fb8a0ff3032e7ecd76d4a7765ed21c1a
smc045 <smc045@stunitas.com>
2022-09-18 18:04:40 +0900

    [GONG-8723]교정학, 또 1위 김지훈

--- GONG-8723/web/index.js
+++ GONG-8723/web/index.js
@@ -31,4 +31,80 @@ $(function() {
         speed: 25000,
         ticker: true,
     });
+
+
+    /* video-js 통한 영상 재생 관련 스크립트 */
+    const options = {
+        sources: [{
+            src: "https://d2j2rcmrtco9nl.cloudfront.net/gongdangipmp/FLV/2021/L307123_01_800k.mp4",
+            type: "video/mp4"
+        }],
+        playbackRates: [.5, .75, 1, 1.25, 1.5],
+        poster: "img/sect06_cover.png",
+        controls: true,
+        preload: "auto",
+        width: 900,
+        height: 522,
+        controlBar: {
+            playToggle: true,
+            pictureInPictureToggle: false,
+            remainingTimeDisplay: true,
+            progressControl: true,
+            qualitySelector: false,
+        }
+    };
+
+    const player = videojs('myVideo', options);
+    const video = document.querySelector("#myVideo");
+    let time = 0;
+    let Interval;
+    let video_status = false;
+
+    var video_timer = function() {
+        //console.log('time', time);
+        if(time > 10) {
+            player.pause();
+            $('.sect06 .cont_pop').css('display', 'block');
+
+            clearInterval(Interval);    //Interval 동작을 중지
+
+        } else {
+            time++;
+        }
+    }
+
+    video.addEventListener("click", function() {
+        // console.log('currentTime', player.currentTime());
+
+        if(video_status === false) {
+            if(player.currentTime() <= 10) {
+                // console.log('11111');
+                Interval = setInterval(video_timer, 1000);   //1초마다 timer 동작
+                video_status = true;
+
+            } else {
+                // console.log('22222');
+                clearInterval(Interval);    //Interval 동작을 중지)
+                video_status = false;
+
+                player.pause();
+                $('.sect06 .cont_pop').css('display', 'block');
+            }
+
+        } else if(video_status === true) {
+            clearInterval(Interval);    //Interval 동작을 중지)
+            video_status = false;
+
+            if(player.currentTime() > 10) {
+                player.pause();
+                $('.sect06 .cont_pop').css('display', 'block');
+            }
+        }
+    });
+
+    $('.btn_close').on('click', function() {
+        $('.sect06 .cont_pop').css('display', 'none');
+        clearInterval(Interval);    //Interval 동작을 중지)
+    });
 });
+

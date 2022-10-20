9d26053875b884f448b79650bfb23d1daaf2a386
smc045 <smc045@stunitas.com>
2022-10-20 18:24:02 +0900

    [CONPR-32]커넥츠 프랩

--- web/CONPR-32/index.js
+++ web/CONPR-32/index.js
@@ -4,4 +4,15 @@ $(function() {
         console.log('this is CONPR-32');
 
     }, 100);
+
+    $('.sect04 ul').bxSlider({
+        pager: false,
+        controls: false,
+        auto: true,
+        pause: 3000,
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 384,
+        slideMargin: 24
+    });
 });

7bda449c6d05c32d0f21a47dd7cc9b5c18a1001c
smc045 <smc045@stunitas.com>
2022-10-21 12:16:10 +0900

    [CONPR-32]커넥츠 프랩

--- web/CONPR-32/index.js
+++ web/CONPR-32/index.js
@@ -3,6 +3,8 @@ $(function() {
     setTimeout(function() {
         console.log('this is CONPR-32');
 
+        pie_draw(95, '.cont_pie:eq(0)');
+        pie_draw(87, '.cont_pie:eq(1)');
     }, 100);
 
     $('.sect04 ul').bxSlider({
@@ -15,4 +17,30 @@ $(function() {
         slideWidth: 384,
         slideMargin: 24
     });
+
+    /* 원형 차트 script */
+    var pie_draw = function(max, className) {
+        let i = 1;
+
+        // max 수치까지 원형 그래프 그려질 수 있도록 setInterval 호출
+        let func1 = setInterval(function() {
+            if(i < max) {
+                pie_color(i, className);
+                i++;
+
+            } else {
+                clearInterval(func1);
+            }
+        },10);
+    };
+
+    /* 원형 차트 그리기 */
+    var pie_color = function(i, className) {
+        $(className).css({
+            "background": "conic-gradient(#172C8D 0% "+i+"%, #F2F2F2 "+i+"% 100%)"
+        });
+    };
+
+
+
 });
